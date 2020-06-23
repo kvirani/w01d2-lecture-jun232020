fc49bbb37d0af5435962fff0a19ff46d5ecd12a9
Khurram Virani <kvirani@gmail.com>
2020-06-23 11:43:49 -0700

    add problem statement to top

--- sum.js
+++ sum.js
@@ -1,3 +1,5 @@
+// Write a node program/app/script/software program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them.
+
 let input = process.argv.slice(2);
 
 let sum = function(numbers) {
